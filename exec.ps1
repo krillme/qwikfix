aa096c2269b269bc8f40a2bd2eb057c5056cbf88
krillme <77764456+krillme@users.noreply.github.com>
2025-08-04 02:38:05 -0700

    Update exec.ps1

--- exec.ps1
+++ exec.ps1
@@ -1,3 +1,10 @@
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "Restarting script as administrator..." -ForegroundColor Yellow
+    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
 Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/cleanup-image", "/restorehealth" -Wait -NoNewWindow
 Start-Process -FilePath "sfc.exe" -ArgumentList "/scannow" -Wait -NoNewWindow
 cmd.exe /c "echo Y | chkdsk C: /r"
