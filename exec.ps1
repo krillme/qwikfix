988e0688095d1df0e970e2e139ce3fa2359f1dca
krillme <77764456+krillme@users.noreply.github.com>
2025-08-04 02:36:13 -0700

    Update exec.ps1

--- exec.ps1
+++ exec.ps1
@@ -1,5 +1,4 @@
 Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/cleanup-image", "/restorehealth" -Wait -NoNewWindow
 Start-Process -FilePath "sfc.exe" -ArgumentList "/scannow" -Wait -NoNewWindow
-chkdsk C: /r
-Write-Host "Script finished." -ForegroundColor Green
-Read-Host
+cmd.exe /c "echo Y | chkdsk C: /r"
+Write-Host "Reboot your computer." -ForegroundColor Green
