3a188a46c70254ec12ad61ddaa55004d0a19b831
krillme <77764456+krillme@users.noreply.github.com>
2025-08-04 02:47:40 -0700

    Update exec.ps1

--- exec.ps1
+++ exec.ps1
@@ -1,10 +1,3 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    Exit
-}
-
 Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/cleanup-image", "/restorehealth" -Wait -NoNewWindow
 Start-Process -FilePath "sfc.exe" -ArgumentList "/scannow" -Wait -NoNewWindow
 cmd.exe /c "echo Y | chkdsk C: /r"
